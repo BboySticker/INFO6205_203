953d0a5e4edb38deff2665c0fef2122c034fcd1a
Xinyu Zhang <zhang.xinyu3@husky.neu.edu>
2019-12-02 21:39:29 -0500

    pushed all staff

--- src/main/java/edu/neu/coe/info6205/life/ga/Selection.java
+++ src/main/java/edu/neu/coe/info6205/life/ga/Selection.java
@@ -1,4 +1,9 @@
 package edu.neu.coe.info6205.life.ga;
 
+/**
+ * This class will evaluate the fitness of each element of the population
+ * and build a mating pool.
+ */
 public class Selection {
+
 }
--- src/main/java/edu/neu/coe/info6205/mystaff/Main.java
+++ src/main/java/edu/neu/coe/info6205/mystaff/Main.java
@@ -1,4 +1,23 @@
-package edu.neu.coe.info6205.mystaff;
-
-public class Main {
-}
+//package edu.neu.coe.info6205.mystaff;
+//
+//public class Main {
+//    private GOL gol;
+//
+//    void setup() {
+//        // setup the canvas
+//        size(640, 360);
+//        gol = new GOL();
+//    }
+//
+//    void draw() {
+//        background(255);
+//
+//        gol.generate();
+//        gol.display();
+//    }
+//
+//    // reset board when mouse is pressed
+//    void mousePressed() {
+//        gol.init();
+//    }
+//}
